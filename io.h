253fe2d95615d22cf20957fe233dbb86332862be
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-06-21 23:19:33 +0300

    SDL functions and main loop added

--- io.h
+++ io.h
@@ -1,13 +1,9 @@
 #ifndef __IO_
 #define _IO_
 
-// #ifndef LINUX
-// #include "SDL/include/SDL.h"                            
-// #include "SDL/SDL_GfxPrimitives/SDL_gfxPrimitives.h"
-// #else
-// #include <SDL/SDL.h>                                  
-// #include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"    
-// #endif
+#include "SDL/include/SDL.h"                            
+#include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"
+
 #pragma comment (lib, "SDL/lib/SDL.lib")
 #pragma comment (lib, "SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
 
---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include "game.h"
+
+int main(int argc, char** argv)
+{
+    IO io;
+    int screenHeight = io.getScreenHeight();
+
+    Pieces pieces;
+    Board board(&pieces, screenHeight);
+    Game game(&board, &pieces, &io, screenHeight);
+
+    unsigned long time = SDL_GetTicks();
+
+    while(!io.isKeyDown(SDLK_ESCAPE))
+    {
+        io.clearScreen();
+        game.drawScene();
+        io.updateScreen();
+
+        int key = io.pollkey();
+
+        switch (key)
+        {
+            case SDLK_RIGHT:
+                /* code */
+                break;
+
+            case SDLK_LEFT:
+                /* code */
+                break;
+            
+            case SDLK_DOWN:
+                /* code */
+                break;
+            
+            case SDLK_x:
+                /* code */
+                break;
+            
+            case SDLK_z:
+                /* code */
+                break;
+
+            default:
+                break;
+        }
+
+        unsigned long time2 = SDL_GetTicks();
+
+        if((time2 - time) > WAIT_TIME)
+        {
+            if(board.isPossibleMovement(game.posX, game.posY + 1, game.piece, game.rotation))
+                game.posY++;
+            else {
+                board.storePiece(game.posX, game.posY, game.piece, game.rotation);
+                board.deletePossibleLines();
+
+                if(board.isGameOver())
+                {
+                    io.getkey();
+                    exit(0);
+                }
+
+                game.createNewPiece();
+            }
+
+            time = SDL_GetTicks();
+        }
+    }
+
+    return 0;
+}
